85bb4034961426b9c0d130155483d74a435ef19d
isfopo <55164207+isfopo@users.noreply.github.com>
2020-09-24 18:55:17 -0400

    adds Play() method to Quantize class

--- Assets/Scripts/Quantize.cs
+++ Assets/Scripts/Quantize.cs
@@ -7,6 +7,7 @@ public class Quantize : MonoBehaviour
 {
     public int BPM;
     private uint _128noteCount;
+    private int divisor;
 
     void OnEnable()
     {
@@ -20,6 +21,57 @@ public class Quantize : MonoBehaviour
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        Debug.Log("Restart scene");
+        StartCoroutine(Count(1.875f / BPM));
+    }
+
+    private IEnumerator Count(float SecondsToWait)
+    {
+        _128noteCount++;
+        yield return new WaitForSeconds(SecondsToWait);
+        StartCoroutine(Count(SecondsToWait));
+    }
+
+    // ===================================================
+
+    public void Play(AudioSource source, string beatCode)
+    {
+        divisor = GetDivisorFromCode(beatCode);
+        StartCoroutine(WaitToPlay(source, divisor));
+    }
+
+    private IEnumerator WaitToPlay(AudioSource source, int divisor)
+    {
+        yield return new WaitUntil(() => _128noteCount % divisor == 0);
+
+        source.Play();
+    }
+
+    public void Stop(AudioSource source, string beatCode)
+    {
+        divisor = GetDivisorFromCode(beatCode);
+        StartCoroutine(WaitToStop(source, divisor));
+    }
+
+    private IEnumerator WaitToStop(AudioSource source, int divisor)
+    {
+        yield return new WaitUntil(() => _128noteCount % divisor == 0);
+
+        source.Stop();
+    }
+
+    private int GetDivisorFromCode(string beatCode)
+    {
+        if (beatCode[beatCode.Length - 1] == 'n')
+        {
+            return 128 / int.Parse(beatCode.Substring(0, beatCode.Length - 1));
+        }
+        else if (beatCode[beatCode.Length - 1] == 'b')
+        {
+            return 128 * int.Parse(beatCode.Substring(0, beatCode.Length - 1));
+        }
+        else
+        {
+            throw new System.Exception("Code must end with 'n' for note or 'b' for bar");
+        }
     }
 }
