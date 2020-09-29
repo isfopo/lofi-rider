46c15a1c528dbce83cc3d0d73cb4909bf6354569
isfopo <55164207+isfopo@users.noreply.github.com>
2020-09-28 23:13:20 -0400

    adds a metronome

---
+++ Assets/Scripts/SoundManagers/ClickManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    public AudioSource click;
+    public Quantize quantize;
+    private int currentLoop = 0;
+
+    void Awake()
+    {
+        Quantize quantize = GetComponentInParent<Quantize>();
+        AudioSource click = GetComponent<AudioSource>();
+    }
+
+    [Button]
+    void StartClick()
+    {
+
+        quantize.Play(click, "4n", true);
+    }
+}
