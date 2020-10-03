c6635f1cda613b30b2ccf372d43f8d188620c5e8
isfopo <55164207+isfopo@users.noreply.github.com>
2020-10-03 14:49:33 -0400

    nothing moves until game has started

--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -5,10 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class GameStarter : MonoBehaviour
 {
+    public GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
+            gameManager.GameHasStarted = true;
             SceneManager.LoadScene("TestScene");
         }
     }
