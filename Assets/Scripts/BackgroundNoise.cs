30ca288c83c1125b449c0e75305293ce42671193
isfopo <55164207+isfopo@users.noreply.github.com>
2020-10-03 18:40:08 -0400

    creates a "tv static" background to start scene

---
+++ Assets/Scripts/BackgroundNoise.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class BackgroundNoise : MonoBehaviour
+{
+    public int width = 256;
+    public int height = 256;
+
+    public Vector2 redRange;
+    public Vector2 greenRange;
+    public Vector2 blueRange;
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = GenerateTexture();
+    }
+
+    Texture2D GenerateTexture()
+    {
+        Texture2D texture = new Texture2D(width, height);
+
+        for ( int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                texture.SetPixel(x, y, CalculateColor());
+            }
+        }
+        texture.Apply();
+        return texture;
+    }
+
+    Color CalculateColor()
+    {
+        float sample = Random.value;
+        return new Color(
+            Mathf.SmoothStep(redRange.x, redRange.y, sample),
+            Mathf.SmoothStep(greenRange.x, greenRange.y, sample),
+            Mathf.SmoothStep(blueRange.x, blueRange.y, sample)
+        );
+    }
+}
