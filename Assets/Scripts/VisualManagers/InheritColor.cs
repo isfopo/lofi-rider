ea750d8c706aef637bf331743699c8bfe732993b
isfopo <55164207+isfopo@users.noreply.github.com>
2020-10-04 12:11:44 -0400

    creates scripts needed to create dynamic color thems

---
+++ Assets/Scripts/VisualManagers/InheritColor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class InheritColor : MonoBehaviour
+{
+    public enum ThemeSelection
+    {
+        foreground1,
+        foreground2,
+        background1,
+        background2
+    }
+
+    public SpriteRenderer spriteRenderer;
+    public LevelDesigner levelDesigner;
+    public ThemeSelection themeSelection;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        levelDesigner = GetComponentInParent<LevelDesigner>();
+        Debug.Log(themeSelection);
+        SelectTheme(themeSelection);
+    }
+
+    void SelectTheme(ThemeSelection themeSelection)
+    {
+        switch (themeSelection)
+        {
+            case ThemeSelection.foreground1:
+                spriteRenderer.color = levelDesigner.foregroundColor1;
+                break;
+            case ThemeSelection.foreground2:
+                spriteRenderer.color = levelDesigner.foregroundColor2;
+                break;
+            case ThemeSelection.background1:
+                spriteRenderer.color = levelDesigner.backgroundColor1;
+                break;
+            case ThemeSelection.background2:
+                spriteRenderer.color = levelDesigner.backgroundColor2;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/VisualManagers/LevelDesigner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDesigner : MonoBehaviour
+{
+    public Color foregroundColor1;
+    public Color foregroundColor2;
+    public Color backgroundColor1;
+    public Color backgroundColor2;
+}
