0f9838705e0401cc2f696cfd8f433060c90253d9
isfopo <55164207+isfopo@users.noreply.github.com>
2020-10-03 22:53:02 -0400

    out of bounds bug fix

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float jumpHeight;
 
     public GameManager gameManager;
+    public GameObject Camera;
 
     public float BaseSpeed;
     public float SpeedMultiplier;
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         gameManager = FindObjectOfType<GameManager>();
+        Camera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     void Update()
@@ -24,6 +26,11 @@ public class PlayerController : MonoBehaviour
         if (Input.GetAxis("Vertical") > 0 && GameObject.FindGameObjectWithTag("Player").transform.position.y < 0.1 )
         {
             rb.velocity = new Vector3(1, jumpHeight, 0);
+        }
+
+        if (Camera.transform.position.y - transform.position.y > 5)
+        {
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
         }
     }
 }
