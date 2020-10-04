b41414331b54df906f0cc843632e29a2039a65ad
isfopo <55164207+isfopo@users.noreply.github.com>
2020-10-03 20:54:40 -0400

    play "radio static" at start from random point in clip

---
+++ Assets/Scripts/SoundManagers/RadioStaticManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioStaticManager : MonoBehaviour
+{
+    public AudioSource RadioStatic;
+
+    void Start()
+    {
+        RadioStatic = GetComponent<AudioSource>();
+
+        RadioStatic.time = Random.Range(0, RadioStatic.clip.length);
+        RadioStatic.Play();
+    }
+}
