5814e11f6194e32438a06a248be0626bec7c8638
isfopo <55164207+isfopo@users.noreply.github.com>
2020-10-26 17:12:27 -0400

    moves ControlMixer.cs to script folder

---
+++ Assets/Scripts/ControlMixer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ControlMixer : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioMixer mixer = Resources.Load("MasterMixer") as AudioMixer;
+        GetComponent<AudioSource>().outputAudioMixerGroup = mixer.FindMatchingGroups("Master")[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
