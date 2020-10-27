bb361eb3e42ea8bba1f7cec79671118792eda848
isfopo <55164207+isfopo@users.noreply.github.com>
2020-10-27 14:41:59 -0400

    adds progress bar

---
+++ Assets/Scripts/UIControllers/ProgressBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    private Slider _slider;
+    public GameManager gameManager;
+    public BackgroundMusicManager backgroundMusicManager;
+
+    void Awake()
+    {
+        _slider = gameObject.GetComponent<Slider>();
+        gameManager = FindObjectOfType<GameManager>();
+        backgroundMusicManager = FindObjectOfType<BackgroundMusicManager>();
+
+        _slider.maxValue = backgroundMusicManager.ScoreForNext[backgroundMusicManager.ScoreForNext.Length - 1];
+    }
+
+    void Update()
+    {
+        _slider.value = gameManager.Score;
+    }
+}
