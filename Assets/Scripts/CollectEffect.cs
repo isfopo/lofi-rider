379d97a0b9335ed43c120db27a77be9d968db64f
isfopo <55164207+isfopo@users.noreply.github.com>
2020-10-28 21:46:25 -0400

    adds effect when collectable is collected

---
+++ Assets/Scripts/CollectEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectEffect : MonoBehaviour
+{
+    public ParticleSystem CollectParticle;
+    public AudioSource CollectSound;
+
+    public void CreateEffect()
+    {
+        CollectParticle.Play();
+        CollectSound.Play();
+
+        Destroy(gameObject, CollectParticle.main.duration);
+    }
+}
---
+++ Assets/Scripts/DestroyCollectable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCollectable : MonoBehaviour
+{
+    public GameObject CollectEffect;
+    private GameObject instance;
+
+    public void Collect()
+    {
+        instance = Instantiate(CollectEffect, transform.position, transform.rotation);
+
+        instance.GetComponent<CollectEffect>().CreateEffect();
+
+        Destroy(gameObject);
+    }
+}
